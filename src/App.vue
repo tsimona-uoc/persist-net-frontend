a5f3b1007cab6064e3bdc79a8a0719172e8cc18c
tesian99 <simon.antonio.temis@gmail.com>
2026-03-14 17:07:24 +0100

    Agregada ventana (mockup) del login

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  
+    <AppToolbar></AppToolbar>
+    <div class="w-screen h-screen flex">
+        <LoginView class="m-auto"></LoginView>
+    </div>
 </template>
 
 <script setup lang="ts">
 
-
+import LoginView from './components/login/LoginView.vue';
 
 </script>
